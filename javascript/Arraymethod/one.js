0fc9b9b9569d7595e795382bead6c90dd63040ce
haseena <haseena22112020@gmail.com>
2022-10-09 23:27:26 -0700

    okkkk

---
+++ javascript/Arraymethod/one.js
@@ -0,0 +1,18 @@
+let arr=[10,20,30,40,50,"haseena","false"]
+//arr.push(50)
+//arr.pop(10)
+//console.log(arr)
+//arr.length
+//console.log(arr)
+
+//arr.shift()
+//console.log(arr)
+//arr.unshift(100)
+//console.log(arr)
+console.log(arr)
+//arr.slice(0,4)
+console.log(arr.slice(3,5))
+let text="hello world,welcome to the universe";
+
+let result=text.indexOf("world")
+console.log(result)
